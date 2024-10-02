2866e02e5ccc7a48e0c9390a0f8893d4837216e8
ajei1007 <yyurii.k.lee@gmail.com>
2024-10-02 13:25:55 +0200

    change the validation

--- src/app/components/Chart.tsx
+++ src/app/components/Chart.tsx
@@ -70,7 +70,7 @@ export function ChartComponent({ url, type }: ChartComponentProps) {
                     .line()
                     .encode('x', 'Date')
                     .encode('y', 'Value');
-            } else if (type === "column") {
+            } else {
                 chart
                     .interval()
                     .encode('x', 'Date')
